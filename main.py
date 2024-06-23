aaa620c21f88ac179aa87170c013fe98ec8716c5
Kohei Morino <kohei.april20@gmail.com>
2024-06-24 06:31:51 +0900

    Applied Tokyo's timezone

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
+import pytz
 from datetime import datetime
 from fetcher import scrape_fiscal_reports_by_date, download_pdfs
 from vectorstore import upload_new_files
 
 args = sys.argv
 
-date = datetime.today()
+local_timezone = pytz.timezone('Asia/Tokyo')
+date = datetime.now(local_timezone)
 if len(args) > 1:
     date = datetime.strptime(args[1], "%Y%m%d")
 
